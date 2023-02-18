29fe49c6064837fb2b578fe21b68cf3d7e347da9
Adanan-chouhan <adananchouhan@gmail.com>
2023-02-18 13:26:46 +0530

    yahoo baba

---
+++ js/33-Arrays.js
@@ -0,0 +1,10 @@
+document.write("<center><h1>Arrays<h1><center>");
+document.write("<br>");
+var ary=[10,20,30,40,50];
+document.write("<ul>");
+for(a = 0; a <= 4; a++){
+    document.write("<li>"+ary[a]+"</li>");
+}
+
+document.write("</ul>");
+document.write("end")
