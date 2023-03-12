e09079f00cf4533d4fb8c3ddd8c7fe13fef212b8
Adanan-chouhan <adananchouhan@gmail.com>
2023-03-12 15:57:57 +0530

    yahoo baba

---
+++ js/38-pop-and-push-method.js
@@ -0,0 +1,8 @@
+document.write("<center><h1>POP AND PUSH METHOD<h1><center>");
+document.write("<br>");
+var  a = ["adnan","fayyaz","danis","arbaaz","barkat"];
+document.write(a + "<br>"+"<br>");
+a.pop();
+document.write( a + "<br>" + "<br>");
+a.push("ayyaz");
+document.write(a + "<br>");
