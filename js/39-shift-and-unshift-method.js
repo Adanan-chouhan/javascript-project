17458c20900cf415f1129b605fcdc7b33ed64029
Adanan-chouhan <adananchouhan@gmail.com>
2023-03-13 02:01:53 +0530

    yahoo baba

---
+++ js/39-shift-and-unshift-method.js
@@ -0,0 +1,10 @@
+document.write("<center><h1> SHIFT AND UNSHIFT METHOD<h1><center>");
+document.write("<br>");
+var  a = ["ayyaz","adnan","danis","arbaaz","fayyaz"];
+document.write(a + "<br>"+"<br>");
+a.shift();
+document.write( a + "<br>" + "<br>");
+a.unshift("sulaman");
+document.write(a + "<br>")
+
+ 
