5b3703011606c5db1c34057664c2c2835da4ba69
Adanan-chouhan <adananchouhan@gmail.com>
2023-03-14 00:23:49 +0530

    yahoo baba

---
+++ js/40-concat-and-join-method.js
@@ -0,0 +1,8 @@
+document.write("<center><h1>CONCAT AND JOIN METHOD<center><h1>");
+document.write("<br>");
+var a = ["adnan","fayyaz"];
+var b = ["arbaaz","mujahid"];
+var c = a.concat(b);
+document.write(c + "<br>" + "<br>");
+var d = c.join(" - ")
+document.write(d);
