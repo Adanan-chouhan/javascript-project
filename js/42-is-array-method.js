5da75bb8130389b7ae5208034123086eaeb67325
Adanan-chouhan <adananchouhan@gmail.com>
2023-03-18 03:31:06 +0530

    yahoo baba

---
+++ js/42-is-array-method.js
@@ -0,0 +1,7 @@
+document.write("<center><h1>IS ARRAY METHOD<h1><center>");
+document.write("<br>");
+//var  a = ["adnan","arbaaz","fayyaz","mujahid"] ;
+var  a = 15;
+document.write(a + "<br>" + "<br>");
+var b = Array.isArray(a);
+document.write(b);
