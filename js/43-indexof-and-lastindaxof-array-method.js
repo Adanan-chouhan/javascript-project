d4789cb36e825db89954ba2fa5f2641689cbaf02
Adanan-chouhan <adananchouhan@gmail.com>
2023-03-19 00:02:54 +0530

    yahoo baba

---
+++ js/43-indexof-and-lastindaxof-array-method.js
@@ -0,0 +1,10 @@
+document.write("<center><h1>INDEXOF AND LASTINDEXOF<h1><center>");
+document.write("<br>");
+var a = ["adnan","danis","arbaaz","adnan","fayyaz","mujahid"];
+document.write(a + "<br>" + "<br>")
+var b = a.indexOf("adnan");
+document.write(b);
+
+document.write("<hr>");
+var c = a.lastIndexOf("adnan",4);
+document.write(c);
