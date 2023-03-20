a99e284fcb1974597ff974279d4a81247e0518a5
Adanan-chouhan <adananchouhan@gmail.com>
2023-03-20 23:37:25 +0530

    yahoo baba

---
+++ js/44-includes-array-method.js
@@ -0,0 +1,6 @@
+document.write("<center><h1>INCLUDES ARRAY METHOD<h1><center>");
+document.write("<br>");
+var a = ["adnan","danis","arbaaz","fayyaz","mujahid"];
+document.write(a + "<br>" + "<br>");
+var b = a.includes("arbaaz");
+document.write(b);
