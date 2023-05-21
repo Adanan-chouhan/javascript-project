3531c6974741686cb868ec9489877bbc1cb279d2
Adanan-chouhan <adananchouhan@gmail.com>
2023-05-21 18:37:00 +0530

    Yahoo baba

---
+++ js/51-object-toutorial-II.js
@@ -0,0 +1,11 @@
+document.write("<h1><center>object-toutorial-II<center><h1>");
+document.write("<br>");
+var a = new Object();
+
+a.firstName = 'adnan';
+a.lastName = 'chouhan';
+a.age = 19;
+a.salary = 35000;
+
+document.write(a.firstName + " ");
+document.write(a['lastName']);
