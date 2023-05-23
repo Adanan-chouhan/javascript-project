6ee39dcac2312917ee76510ae2cef598eeb2e4b0
Adanan-chouhan <adananchouhan@gmail.com>
2023-05-23 23:48:12 +0530

    content variable

---
+++ js/53-const-variable-with-array-and-object.js
@@ -0,0 +1,15 @@
+document.write("<center><h1>CONST VARIABALE WITH ARRAY AND OBJECT<h1><center>");
+document.write("<br>");
+const a = [10 , 20 , 30];
+a[1] = 23;
+a[2] = 20;
+document.write(a);
+document.write("<br>");
+
+const b = {
+    name : "adnan",
+    age : 19
+}
+b.name = "arbaaz"
+b.age = 17
+document.write(b.name + "  " + b.age)
