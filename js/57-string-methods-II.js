6e0d057caf93942a6203e52c7eec0fb5840d0deb
Adanan-chouhan <adananchouhan@gmail.com>
2023-05-31 23:42:46 +0530

    string method

---
+++ js/57-string-methods-II.js
@@ -0,0 +1,79 @@
+document.write("<center><h1>STRING METHODS II </h1></center>");
+document.write("<br>");
+document.write("<h1>13-charAt</h1>");
+var str = "my name is adnan chouhan";
+var a = str.charAt(3);
+document.write(a);
+document.write("<hr>");
+
+document.write("<h1>14-charCodeAt</h1>");
+var b = "my name is adnan chouhan";
+var c = b.charCodeAt(4);
+document.write(c);
+document.write("<hr>");
+
+document.write("<h1>15-fromCharCode</h1>");
+var d = String.fromCharCode(65);
+document.write(d);
+document.write("<hr>");
+
+document.write("<h1>16-concat</h1>");
+var e  = "my name is adnan chouhan ";
+var f = "i am student of bca";
+var g = e.concat(f);
+document.write(g);
+document.write("<hr>");
+
+document.write("<h1>17-split</h1>");
+var h = "my name is adnan chouhan ";
+var i = e.split("a");
+document.write(i);
+document.write("<hr>");
+
+document.write("<h1>18-repeat</h1>");
+var j = "my name is adnan chouhan ";
+var k = j.repeat(3);
+document.write(k);
+document.write("<hr>");
+
+document.write("<h1>19-slice</h1>");
+var l = "my name is adnan chouhan ";
+var m = l.slice(3,7);
+document.write(m);
+document.write("<hr>");
+
+document.write("<h1>20-substr</h1>");
+var n = "my name is adnan chouhan ";
+var o = n.substr(3,14);
+document.write(o);
+document.write("<hr>");
+
+document.write("<h1>21-substring</h1>");
+var p = "my name is adnan chouhan ";
+var q = p.substring(11,16 );
+document.write(q);
+document.write("<hr>");
+
+document.write("<h1>22-tostring</h1>");
+var r = 50;
+var s = r.toString();
+document.write(s + 20);
+document.write("<hr>");
+
+document.write("<h1>23-valueof</h1>");
+var t = "my name is adnan chouhan ";
+var u = t.valueOf();
+document.write(u);
+document.write("<hr>");
+
+
+
+
+
+
+
+
+ 
+
+
+
