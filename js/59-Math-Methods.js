ff8d82a9476cf9e3a1178687c1fc8bfc683462d0
Adanan-chouhan <adananchouhan@gmail.com>
2023-06-03 23:14:29 +0530

    Math method

---
+++ js/59-Math-Methods.js
@@ -0,0 +1,73 @@
+document.write("<center><h1>MATH METHODS</h1></center>");
+document.write("<br>");
+
+document.write("<h1>ceil</h1>");
+var a = Math.ceil(5.18);
+document.write(a);
+document.write("<hr>");
+
+document.write("<h1>floor</h1>");
+var b = Math.floor(3.17);
+document.write(b);
+document.write("<hr>");
+
+document.write("<h1>round</h1>");
+var c = Math.round(4.60);
+document.write(c);
+document.write("<hr>");
+
+document.write("<h1>trunk</h1>");
+var d = Math.trunc(6.19);
+document.write(d);
+document.write("<hr>");
+
+document.write("<h1>max</h1>");
+var e = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+document.write(e);
+document.write("<hr>");
+
+document.write("<h1>min</h1>");
+var f = Math.min(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+document.write(f);
+document.write("<hr>");
+
+document.write("<h1>sqrt</h1>");
+var g = Math.sqrt(25);
+document.write(g);
+document.write("<hr>");
+
+document.write("<h1>cbrt</h1>");
+var h = Math.cbrt(125);
+document.write(h);
+document.write("<hr>");
+
+document.write("<h1>pow</h1>");
+var i = Math.pow(2,3);
+document.write(i);
+document.write("<hr>");
+
+document.write("<h1>random</h1>");
+var j =  Math.floor(Math.random( ) * 100 + 1);
+document.write(j);
+document.write("<hr>");
+
+document.write("<h1>abs</h1>");
+var k =  Math.abs(2 + 5);
+document.write(k);
+document.write("<hr>");
+
+document.write("<h1>PI</h1>");
+var l =  Math.PI;
+document.write(l);
+document.write("<hr>");
+
+
+
+
+
+
+
+
+
+
+
