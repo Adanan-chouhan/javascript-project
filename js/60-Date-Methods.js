955fcb662ab9cf6c1e113cafdce8dbf07be652d2
Adanan-chouhan <adananchouhan@gmail.com>
2023-06-04 18:46:42 +0530

    date method

---
+++ js/60-Date-Methods.js
@@ -0,0 +1,106 @@
+document.write("<center><h1>DATE METHODS</h1></center>");
+document.write("<br>");
+
+document.write("<h1>to-Date-String</h1>");
+var now = new Date();
+document.write(now.toDateString());
+document.write("<hr>");
+
+document.write("<h1>getDate</h1>")
+var now = new Date();
+document.write(now.getDate());
+document.write("<hr>");
+
+document.write("<h1>getFullYear</h1>")
+var now = new Date();
+document.write(now.getFullYear());
+document.write("<hr>");
+
+
+document.write("<h1>getMonth</h1>")
+var now = new Date();
+document.write(now.getMonth());
+document.write("<hr>");
+
+
+document.write("<h1>getDay</h1>")
+var now = new Date();
+document.write(now.getDay());
+document.write("<hr>");
+
+document.write("<h1>getHours</h1>")
+var now = new Date();
+document.write(now.getHours());
+document.write("<hr>");
+
+document.write("<h1>getMinutes</h1>")
+var now = new Date();
+document.write(now.getMinutes());
+document.write("<hr>");
+
+document.write("<h1>getSeconds</h1>")
+var now = new Date();
+document.write(now.getSeconds());
+document.write("<hr>");
+
+document.write("<h1>getMilliSeconds</h1>")
+var now = new Date();
+document.write(now.getMilliseconds());
+document.write("<hr>");
+
+document.write("<h1>setDate</h1>");
+var a = new Date();
+a.setDate(20);
+document.write(a);
+document.write("<hr>");
+
+document.write("<h1>setFullYear</h1>");
+var a = new Date();
+a.setFullYear(2024);
+document.write(a);
+document.write("<hr>");
+
+document.write("<h1>setMonth</h1>");
+var a = new Date();
+a.setMonth(3);
+document.write(a);
+document.write("<hr>");
+
+document.write("<h1>setHours</h1>");
+var a = new Date();
+a.setHours(10);
+document.write(a);
+document.write("<hr>");
+
+document.write("<h1>setMinutes</h1>");
+var a = new Date();
+a.setMinutes(19);
+document.write(a);
+document.write("<hr>");
+
+document.write("<h1>setSeconds</h1>");
+var a = new Date();
+a.setSeconds(45);
+document.write(a);
+document.write("<hr>");
+
+document.write("<h1>setMilliseconds</h1>");
+var a = new Date();
+a.setMilliseconds(156);
+document.write(a);
+document.write("<hr>");
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
