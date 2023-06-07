4df767938aab389a68f8485d7b817a32de02c41c
Adanan-chouhan <adananchouhan@gmail.com>
2023-06-07 23:49:20 +0530

    Dom targating methods

---
+++ js/62-DOM-Targeting-Methods.js
@@ -0,0 +1,19 @@
+var element;
+var element = document;
+
+console.log(element);
+
+// element = document.all[9]
+// element = document.head
+// element = document.title
+// element = document.body
+// element = document.links
+// element = document.images
+// element = document.forms
+// element = document.doctype
+// element = document.URL
+// element = document.domain
+// element = document.baseURI
+// element = document.getElementById(a1)
+// element = document.getElementsByClassName(a1)
+// element = document.getElementsByTagName(ul)
