92baa66927e157405eecaa61e098ba31b419197a
Adanan-chouhan <adananchouhan@gmail.com>
2023-06-09 23:23:00 +0530

    Dom methods

---
+++ js/64-quaryselector-and-quaryselectorall-method.js
@@ -0,0 +1,7 @@
+document.write("<h1>quaryselector-and-quruselecorall</hb1>");
+
+var a = document.querySelector("#list").innerHTML;
+
+// a = document.querySelectorAll("list h1")[1].innerHTML;
+
+console.log(a);
