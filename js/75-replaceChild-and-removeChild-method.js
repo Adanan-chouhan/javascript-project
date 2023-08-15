7a78cbbe78811637c39dacb6b42a3a47ef351237
Adanan-chouhan <adananchouhan@gmail.com>
2023-08-15 11:47:59 +0530

    yahoo baba

---
+++ js/75-replaceChild-and-removeChild-method.js
@@ -0,0 +1,18 @@
+document.write("<center><h1>replaceChild and removeChild</h1></center>");
+ 
+
+// var newElement = document.createElement("li");
+// var newText = document.createTextNode("adnan-chouhan");
+
+// newElement.appendChild(newText);
+
+// var target = document.getElementById("list");
+// var oldElement = target.children[0];
+
+// target.replaceChild(newElement,oldElement);
+
+
+var a = document.getElementById("list");
+var b = a.children[0];
+
+a.removeChild(f);
